baa15a03055053a3cb321c9453516824218fadf6
lingyundai <lingyundaiseven@gmail.com>
2024-01-22 15:53:29 -0500

    Initial commit

---
+++ src/main/java/com/asimpleapp/asimpleapp/ASimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.asimpleapp.asimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASimpleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asimpleapp/asimpleapp/ASimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimpleapp.asimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
